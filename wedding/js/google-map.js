9aa792564505c739d5c0280041686d2e7d16d17e
LawlietAnshory <iqbalerz.anshory@gmail.com>
2021-11-16 08:25:34 +0700

    Update google-map.js

--- wedding/js/google-map.js
+++ wedding/js/google-map.js
@@ -5,7 +5,7 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+    var myLatlng = new google.maps.LatLng(0.8573351576355484, 99.57001767950784);
     // 39.399872
     // -8.224454
     
